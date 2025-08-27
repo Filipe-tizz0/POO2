92ba0c16ba13d2e942d4f16a2272684bc7ae8682
Filipe-tizz0 <65237611+Filipe-tizz0@users.noreply.github.com>
2025-08-27 20:42:24 -0300

    Add files via upload

---
+++ DLLComDocumentacaoGit/DLLComDocumentacaoGit/Class1.cs
@@ -0,0 +1,11 @@
+﻿namespace DLLComDocumentacaoGit
+{
+    public class Calculos_Areas
+    {
+        public double Area_Paralelepipedo(double raio, double haltura){
+            //Sim querido leitor, altura com 'H' ...
+            return (2 * Math.PI * raio) * (raio + haltura);
+        }
+
+    }
+}
